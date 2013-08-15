4b05f25c823078eafc0fb25d8d117a5024f96d5e
Jerome Etienne <jerome.etienne@gmail.com>
2013-08-15 19:16:51 +0200

    added a package.require.js

--- package.require.js
+++ package.require.js
@@ -1,6 +1,6 @@
 define( [ './threex.nyancat.js'
         , './threex.nyancatrainbow.js'
         , './threex.nyancatsound.js'
-        , './threex.nyancatstar.js'
+        , './threex.nyancatstars.js'
         ], function(){
 });
